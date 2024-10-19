7a9af46c4cb3ec154be7464e4da966ec5803a7f8
Nicolasachury <nicolasachurychavarro23@gmail.com>
2024-10-19 12:51:50 -0500

    segundo codigo html

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-ocument.getElementbyid("calculate").addEventListener("click", function() {
+document.getElementById("calculate").addEventListener("click", function() {
+
     //obtener los valores de los impunt
-    const num1 = parseFloat(Document.getElementbyid("num1").value);
-    const num2 = parseFloat(Document.getElementbyid("num2").value);
+
+    const num1 = parseFloat(document.getElementById("num1").value);
+
+    const num2 = parseFloat(document.getElementById("num2").value);
+
+
 
     //validar si los numeros son validos
+
     if (isNaN(num1) || isNaN(num2)) {
-        document.getElementById("result").innerText = "porfavor escribir el numero validos.";
+
+    document.getElementById("result").innerText = "porfavor escribir el numero validos.";
+
     } else{
-        // Realizar la operacion y mostrar el resultado
-        const sum = num1 + num2;
-        document.getElementById("result").innerText = "resultado:" + sum;
+
+
+
+    // Realizar la operacion y mostrar el resultado
+
+     const sum = num1 + num2;
+
+
+     document.getElementById("result").innerText = "resultado:" + sum;
+
     }
-):
+
+    });
